e7d2b6ca7f83dce7194b45d0a16e4ed43e88bbd4
ppopcn <ppopcn@qq.com>
2018-10-09 18:26:19 +0800

    Update shadowsocks-libev.sh

--- shadowsocks-libev.sh
+++ shadowsocks-libev.sh
@@ -20,23 +20,7 @@ mbedtls_url="https://tls.mbed.org/download/mbedtls-2.12.0-gpl.tgz"
 
 # Stream Ciphers
 ciphers=(
-aes-256-gcm
-aes-192-gcm
-aes-128-gcm
-aes-256-ctr
-aes-192-ctr
-aes-128-ctr
 aes-256-cfb
-aes-192-cfb
-aes-128-cfb
-camellia-128-cfb
-camellia-192-cfb
-camellia-256-cfb
-xchacha20-ietf-poly1305
-chacha20-ietf-poly1305
-chacha20-ietf
-chacha20
-salsa20
 rc4-md5
 )
 # Color
